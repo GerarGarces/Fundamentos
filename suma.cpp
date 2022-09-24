8d240b04d37550d8c54fa19236f13a4481bb3d19
GerarGarces <Fabian.garces.portes@utelvt.edu.ec>
2022-09-24 00:51:30 +0000

    programa suma.cpp modificados con distinta variables

--- suma.cpp
+++ suma.cpp
@@ -1,12 +1,13 @@
+//modificado por gerar garces portes
 #include<iostream>
 using namespace std;
 int main()
 {
-	float x,y,z;
-	cout<<"ingrese primer  numeros x=";cin>>x;
-	cout<<"ingrese el segundo numero y=";cin>>y;
-	z=x+y;
-	cout<<"El resultado es "<<x<<" + "<<y<<"="<<z<<endl;
+	float g,p,z;
+	cout<<"ingrese primer  numeros g=";cin>>g;
+	cout<<"ingrese el segundo numero p=";cin>>p;
+	z=g+p;
+	cout<<"El resultado es "<<g<<" + "<<p<<"="<<z<<endl;
 
 	return 0;
 }
