68f109eb1d00528108e82b956c89eda43512c7a7
GerarGarces <Fabian.garces.portes@utelvt.edu.ec>
2022-09-24 00:02:18 +0000

    programa suma

---
+++ suma.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x,y,z;
+	cout<<"ingrese primer  numeros x=";cin>>x;
+	cout<<"ingrese el segundo numero y=";cin>>y;
+	z=x+y;
+	cout<<"El resultado es "<<x<<" + "<<y<<"="<<z<<endl;
+
+	return 0;
+}
