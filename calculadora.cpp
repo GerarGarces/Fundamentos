05e0b5f512a7b5e06e70aeffe55445f4f066013e
GerarGarces <Fabian.garces.portes@utelvt.edu.ec>
2022-09-24 00:39:50 +0000

    programa calculadora.cpp modificado con variable distinta

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,19 +1,20 @@
+//creado por gerar garces portes
 #include<iostream>
 using namespace std;
 
 int main()
 {
-	float x,y,s,r,d,m;
-	cout<<"ingrese el valor de x="; cin>>x;
-	cout<<"ingrese el valor de y="; cin>>y;
-	s=x+y;
-	r=x-y;
-	m=x*y;
-	d=x/y;
-	cout<<"el resultafo de la suma es"<<x<<"+"<<y<<"="<<s<<endl;
-	cout<<"el resultafo de la resta es"<<x<<"-"<<y<<"="<<r<<endl;
-	cout<<"el resultafo de la multiplicacion es"<<x<<"*"<<y<<"="<<m<<endl;
-	cout<<"el resultafo de la dividion es"<<x<<"/"<<y<<"="<<d<<endl;
+	float g,p,s,r,d,m;
+	cout<<"ingrese el valor de g="; cin>>g;
+	cout<<"ingrese el valor de p="; cin>>p;
+	s=g+p;
+	r=g-p;
+	m=g*p;
+	d=g/p;
+	cout<<"el resultafo de la suma es"<<g<<"+"<<p<<"="<<s<<endl;
+	cout<<"el resultafo de la resta es"<<g<<"-"<<p<<"="<<r<<endl;
+	cout<<"el resultafo de la multiplicacion es"<<g<<"*"<<p<<"="<<m<<endl;
+	cout<<"el resultafo de la dividion es"<<g<<"/"<<p<<"="<<d<<endl;
 
 	return 0;
 }
