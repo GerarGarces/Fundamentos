91fb83f8b19005f7e93b716ade08e2fe3949cece
GerarGarces <Fabian.garces.portes@utelvt.edu.ec>
2022-09-24 04:23:17 +0000

    programa compara.cpp comodificado y diferente variable

--- compara.cpp
+++ compara.cpp
@@ -1,15 +1,16 @@
+//modificado por gerar garces portes
 #include<iostream>
 using namespace std;
 int main()
 {
-	float a,b;
-	cout<<"ingrese a b:"; cin>>a>>b;
-	if(a>b)
+	float g,p;
+	cout<<"ingrese g p:"; cin>>g>>p;
+	if(g>p)
 		{
-	  cout<<a<<" es mayor a "<<b<<endl;
+	  cout<<g<<" es mayor g "<<p<<endl;
 	  }else{
 
-	  cout<<b<<" es mayor a "<<a<<endl;
+	  cout<<p<<" es mayor g "<<g<<endl;
 	  }
 	return 0;
 
