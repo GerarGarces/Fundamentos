8aca5de8a952f111fd788c8d6dbe6803c8f8f1f7
GerarGarces <Fabian.garces.portes@utelvt.edu.ec>
2022-09-24 00:25:27 +0000

    programa contabiliza.cpp

---
+++ contabiliza.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+int  main()
+{
+	int i= 0 ,l;
+	float x,s= 0 ;
+	cout<< " ingrese el limite l= " ; cin>>l;
+	hacer {
+
+	cout<< " ingrese el numero x= " ; cin>>x;
+	yo = yo + 1 ;
+	s=s+x;
+
+
+
+	} mientras (i<l);
+	cout<< " Se ingresaron " <<l<< " numeros " << " que sumaron " <<s<<endl;
+	devolver  0 ;
+
+
+
+}
